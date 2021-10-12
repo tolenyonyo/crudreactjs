49949cdc9363cf1e654b36337069d6039d364056
tolenyonyo <tolenyonyo@gmail.com>
2021-10-12 12:21:25 +0700

    finished

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Crud from './crud';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Crud />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
